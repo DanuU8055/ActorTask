e6c63a11860211eeb008aa231e05e1f5a788156d
Danish Ahmed Khan <137397946+DanuU8055@users.noreply.github.com>
2024-02-12 20:22:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import movieRouter from "./router/router.js"
-import cors from "cors";
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded ({extended:true}))
-app.use(cors());
+
 app.use("/casting",movieRouter);
 
 
